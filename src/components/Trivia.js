80d987b6e315539d52f6f76032df9250fa9f1624
Kristaps Briks <kristaps.briks@inbox.lv>
2022-09-12 01:04:53 +0300

    Start implement question UI

--- src/components/Trivia.js
+++ src/components/Trivia.js
@@ -2,19 +2,28 @@ import React from 'react'
 
 const Trivia = (props) => {
 
-    const questions = props.data.results.map(item =>{
+    const test = props.data.results[0]
+    console.log(test)
+
+    const questions = props.data.results.map(item => {
+
+        const answers = [...item.incorrect_answers, item.correct_answer]
+
+        console.log('answers '+answers)
+
         return (
             <div key={item.question} className='question'>
-                <h1 dangerouslySetInnerHTML={{__html: item.question }}></h1>
+                <h1 dangerouslySetInnerHTML={{ __html: item.question }}></h1>
+                <p key={item.incorrect_answers}>{item.incorrect_answers}</p>
             </div>
         )
     })
 
-  return (
-    <div className='trivia'>
-        {questions}
-    </div>
-  )
+    return (
+        <div className='trivia'>
+            {questions}
+        </div>
+    )
 }
 
 export default Trivia
