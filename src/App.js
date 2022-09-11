42709653d1190588132e116476cc102da2adda22
Kristaps Briks <kristaps.briks@inbox.lv>
2022-09-11 21:15:25 +0300

    Implement start state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import './App.css';
-import blobYellow from './images/yellowblob.png'
-import blobBlue from './images/blueblob.png'
+import blobYellow from './images/yellowblob.png';
+import blobBlue from './images/blueblob.png';
+import { useState } from 'react';
 
 function App() {
+
+  const [start, setStart] = useState(false);
+
+  function startGame() {
+    return setStart(true);
+  }
+
   return (
     <div className='app'>
       <div className="start">
         <h1 className='heading'>Quizzical</h1>
         <p className='paragraph'>Play and improve your trivia skills</p>
-        <button>Start Quiz</button>
-        <img className='top-right' src={blobYellow} />
-        <img className='bottom-left' src={blobBlue} />
+        <button className='button' onClick={startGame}>Start Quiz</button>
+        <img className='top-right' src={blobYellow} alt='Yellow blob' />
+        <img className='bottom-left'  src={blobBlue} alt='Blue blob' />
       </div>
     </div>
 
